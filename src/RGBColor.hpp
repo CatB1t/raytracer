12985a240543b2f478ce17b249f81b3c072c89eb
CatB1t <skytedits@gmail.com>
2024-07-09 11:27:39 +0300

    Add operator* to RGBColor

--- src/RGBColor.hpp
+++ src/RGBColor.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <algorithm>
 
 using PixelColorT = uint8_t;
 
@@ -9,4 +10,18 @@ struct RGBColor
     PixelColorT r = 0;
     PixelColorT g = 0;
     PixelColorT b = 0;
+
+    RGBColor operator*(float val) const
+    {
+        return {
+            static_cast<PixelColorT>(std::clamp(static_cast<int>(r * val), 0, 255)),
+            static_cast<PixelColorT>(std::clamp(static_cast<int>(g * val), 0, 255)),
+            static_cast<PixelColorT>(std::clamp(static_cast<int>(b * val), 0, 255)),
+            };
+    };
+};
+
+inline RGBColor operator*(float val, const RGBColor& color)
+{
+    return color * val;
 };
