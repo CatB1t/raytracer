f5c03dccd16e9b7952487bf3d9be4c7d52583fb6
CatB1t <skytedits@gmail.com>
2024-07-14 00:43:28 +0300

    Add operator+ for RGBColor

--- src/RGBColor.hpp
+++ src/RGBColor.hpp
@@ -17,6 +17,14 @@ struct RGBColor {
         static_cast<PixelColorT>(std::clamp(static_cast<int>(b * val), 0, 255)),
     };
   };
+
+  RGBColor operator+(const RGBColor& other) const {
+    return {
+        static_cast<PixelColorT>(std::clamp(r + other.r, 0, 255)),
+        static_cast<PixelColorT>(std::clamp(g + other.g, 0, 255)),
+        static_cast<PixelColorT>(std::clamp(b + other.b, 0, 255)),
+    };
+  };
 };
 
 inline RGBColor operator*(float val, const RGBColor &color) {
