5622094e00d8e47c3aa01adca795abf68e2302c0
CatB1t <skytedits@gmail.com>
2024-07-14 00:42:43 +0300

    Update premake file

--- premake5.lua
+++ premake5.lua
@@ -2,11 +2,14 @@
 workspace "Raytracer"
   configurations { "Debug", "Release" }
 
+build_path = "build/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
 project "Raytracer"
+  architecture "x86_64"
   kind "ConsoleApp"
   language "C++"
-  targetdir "build/%{cfg.buildcfg}"
-  
+  targetdir (build_path)
+  objdir (build_path .. "/obj")
   files { "src/*.cpp", "src/*.hpp" }
 
   filter {"configurations:Debug"}
